51e7d3d8851be27df29450e73a10bcd06c904649
WolverinDEV <git@mcgalaxy.de>
2023-09-24 20:57:57 +0200

    Adding mode

--- install-driver.ps1
+++ install-driver.ps1
@@ -1,10 +1,11 @@
 $cleanName = "valthrun-driver"
+$mode = "debug"
 
 Write-Host "Stopping & deleting driver"
 sc.exe stop $cleanName
 sc.exe delete $cleanName
 
-$path = "$pwd\..\target\x86_64-pc-windows-msvc\release\$cleanName.sys"
+$path = "$pwd\target\x86_64-pc-windows-msvc\$mode\$cleanName.sys"
 Write-Host "Installing & starting driver ($path)"
 sc.exe create $cleanName type= kernel start= demand error= normal binPath= $path DisplayName= $cleanName
 sc.exe start $cleanName
