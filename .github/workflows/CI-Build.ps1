f1bab1dc118ba2d55b7f78d257b2d77a1b798c7e
WolverinDEV <git@mcgalaxy.de>
2023-10-09 17:50:00 +0200

    Do not require cargo make any more

---
+++ .github/workflows/CI-Build.ps1
@@ -0,0 +1,16 @@
+cargo b -v -r
+if(-not $?) {
+    Write-Host "Failed to build driver"
+    exit 1
+}
+
+cp ./target/x86_64-pc-windows-msvc/release/driver.dll ./target/x86_64-pc-windows-msvc/release/valthrun-driver.sys
+if(-not $?) {
+    Write-Host "Failed to copy driver"
+    exit 1
+}
+
+Sign-Driver.ps1 ./target/x86_64-pc-windows-msvc/release/valthrun-driver.sys
+if(-not $?) {
+    exit 1
+}
