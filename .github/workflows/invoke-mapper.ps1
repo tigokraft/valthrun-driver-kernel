fac0fd0954bee4c9a513b629bbf017b0a3eb92c2
WolverinDEV <git@did.science>
2024-02-27 13:45:34 +0100

    Updated CI script

---
+++ .github/workflows/invoke-mapper.ps1
@@ -0,0 +1,18 @@
+$Headers = @{
+    "Accept"               = "application/vnd.github+json"
+    "Authorization"        = "Bearer $env:UEFI_MAPPER_GITHUB_TOKEN"
+    "X-GitHub-Api-Version" = "2022-11-28"
+}
+$Payload = @{
+    "event_type"     = "driver_updated"
+    "client_payload" = @{
+        "driver_authorization" = "$env:DRIVER_GITHUB_TOKEN"
+        "driver_url"           = "https://api.github.com/repos/Valthrun/valthrun-driver/actions/artifacts/$env:DRIVER_ARTIFACT_ID/zip"
+        "driver_version"       = "$env:GITHUB_REF_NAME-$($env:GITHUB_SHA.Substring(0, 7))"
+    }
+}
+Invoke-WebRequest -Uri "https://api.github.com/repos/Valthrun/valthrun-uefi-mapper/dispatches" `
+    -Method POST `
+    -Headers $Headers `
+    -Body $(ConvertTo-Json $Payload)
+    
