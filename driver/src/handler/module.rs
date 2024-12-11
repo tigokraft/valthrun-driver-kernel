514c6d1f1fa45eb9e52e2893640d1b22008f2bdd
WolverinDEV <git@did.science>
2024-12-11 23:55:21 +0900

    fix(driver): filter dead process to prevent 'process is ubiquitous'

--- driver/src/handler/module.rs
+++ driver/src/handler/module.rs
@@ -17,7 +17,10 @@ use valthrun_driver_protocol::{
     },
 };
 
-use crate::util::kprocess;
+use crate::{
+    offsets::get_nt_offsets,
+    util::kprocess,
+};
 
 pub fn handler_get_processes(command: &mut DriverCommandProcessList) -> anyhow::Result<()> {
     let buffer = unsafe {
@@ -34,6 +37,23 @@ pub fn handler_get_processes(command: &mut DriverCommandProcessList) -> anyhow::
 
     command.process_count = 0;
     kprocess::iter(|process| {
+        {
+            #[allow(non_snake_case)]
+            let EPROCESS_ThreadListHead = get_nt_offsets().EPROCESS_ThreadListHead;
+
+            let active_threads = unsafe {
+                process.eprocess()
+                    /* The ActiveThreads comes after the thread list head. Thread list head has a size of 0x10. */
+                    .byte_offset(EPROCESS_ThreadListHead as isize + 0x10)
+                    .cast::<u32>()
+                    .read_volatile()
+            };
+
+            if active_threads == 0 {
+                /* Process terminated / not running */
+                return;
+            }
+        }
         if let Some(output) = buffer.get_mut(command.process_count) {
             output.process_id = process.get_id() as ProcessId;
             output.directory_table_base = process.get_directory_table_base();
