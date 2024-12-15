9c1b2982d241afe39fc9172b577eb69807c61e2f
WolverinDEV <git@did.science>
2024-12-16 02:34:06 +0900

    feat(ci): split uefi upstream invoke and uefi build into two jobs

--- .github/workflows/invoke-mapper.ps1
+++ .github/workflows/invoke-mapper.ps1
@@ -7,8 +7,8 @@ $Payload = @{
     "event_type"     = "driver_updated"
     "client_payload" = @{
         "driver_authorization" = "$env:DRIVER_GITHUB_TOKEN"
-        "driver_url"           = "https://api.github.com/repos/Valthrun/valthrun-kernel-driver/actions/artifacts/$env:DRIVER_ARTIFACT_ID/zip"
-        "driver_version"       = "$env:GITHUB_REF_NAME-$($env:GITHUB_SHA.Substring(0, 7))"
+        "driver_url"           = "https://api.github.com/repositories/673504681/actions/artifacts/$env:DRIVER_ARTIFACT_ID/zip"
+        "driver_version"       = "$($env:GITHUB_SHA.Substring(0, 7))"
     }
 }
 Invoke-WebRequest -Uri "https://api.github.com/repos/Valthrun/valthrun-uefi-mapper/dispatches" `
